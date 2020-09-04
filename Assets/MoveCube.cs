1f493edb01b8478e49d01752fc7199386186200a
Tistou Blomberg <tistou.blomberg@gmail.com>
2020-09-04 17:13:52 +0200

    Level 2 and you win

--- Assets/MoveCube.cs
+++ Assets/MoveCube.cs
@@ -7,8 +7,10 @@ using UnityEngine.SceneManagement;
 public class MoveCube : MonoBehaviour
 {
     public AudioSource pewAS;
+    public AudioSource youWinnAS;
     public Text pointsText;
     int points = 0;
+    float speed = 0.2f;
 
     // Start is called before the first frame update
     void Start()
@@ -27,14 +29,14 @@ public class MoveCube : MonoBehaviour
         }
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            float newX = transform.position.x - 0.1f;
+            float newX = transform.position.x - speed;
             float newY = transform.position.y;
             float newZ = transform.position.z;
             transform.position = new Vector3(newX, newY, newZ);
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            float newX = transform.position.x + 0.1f;
+            float newX = transform.position.x + speed;
             float newY = transform.position.y;
             float newZ = transform.position.z;
             transform.position = new Vector3(newX, newY, newZ);
@@ -43,14 +45,14 @@ public class MoveCube : MonoBehaviour
         {
             float newX = transform.position.x;
             float newY = transform.position.y;
-            float newZ = transform.position.z + 0.1f;
+            float newZ = transform.position.z + speed;
             transform.position = new Vector3(newX, newY, newZ);
         }
         if (Input.GetKey(KeyCode.DownArrow))
         {
             float newX = transform.position.x;
             float newY = transform.position.y;
-            float newZ = transform.position.z - 0.1f;
+            float newZ = transform.position.z - speed;
             transform.position = new Vector3(newX, newY, newZ);
         }
     }
@@ -65,7 +67,14 @@ public class MoveCube : MonoBehaviour
         pewAS.Play();
         if(points == 8)
         {
-            SceneManager.LoadScene("Level2");
+            if (SceneManager.GetActiveScene().name.Equals("Level1"))
+            {
+                SceneManager.LoadScene("Level2");
+            }else if (SceneManager.GetActiveScene().name.Equals("Level2"))
+            {
+                pointsText.text = "YOU WIN!";
+                youWinnAS.Play();
+            }
         }
     }
 }
